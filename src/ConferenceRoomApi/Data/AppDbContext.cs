ad4c32b83d5b9a13c8ab710d58c834c63fe7c9a3
Sergii Smagin <mail.unforgiven@gmail.com>
2026-09-21 00:44:56 -0600

    Add initial database seed data

--- src/ConferenceRoomApi/Data/AppDbContext.cs
+++ src/ConferenceRoomApi/Data/AppDbContext.cs
@@ -41,5 +41,48 @@ public class AppDbContext : DbContext
                 bookingService.BookingId,
                 bookingService.ServiceId
             });
+
+        modelBuilder.Entity<Room>().HasData(
+        new Room
+        {
+            Id = 1,
+            Name = "Room A",
+            Capacity = 50,
+            BaseHourlyRate = 2000
+        },
+        new Room
+        {
+            Id = 2,
+            Name = "Room B",
+            Capacity = 100,
+            BaseHourlyRate = 3500
+        },
+        new Room
+        {
+            Id = 3,
+            Name = "Room C",
+            Capacity = 30,
+            BaseHourlyRate = 1500
+        });
+
+    modelBuilder.Entity<Service>().HasData(
+        new Service
+        {
+            Id = 1,
+            Name = "Projector",
+            Price = 500
+        },
+        new Service
+        {
+            Id = 2,
+            Name = "Wi-Fi",
+            Price = 300
+        },
+        new Service
+        {
+            Id = 3,
+            Name = "Sound",
+            Price = 700
+        });
     }
 }
---
+++ src/ConferenceRoomApi/Migrations/20260921063458_SeedInitialData.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ConferenceRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "BaseHourlyRate", "Capacity", "Name" },
+                values: new object[,]
+                {
+                    { 1, 2000m, 50, "Room A" },
+                    { 2, 3500m, 100, "Room B" },
+                    { 3, 1500m, 30, "Room C" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Projector", 500m },
+                    { 2, "Wi-Fi", 300m },
+                    { 3, "Sound", 700m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
